c4e042e9abb87abdd7f47649d381ad5e89df1a6b
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2025-01-15 14:44:37 +0100

    feat -> global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme.js";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Montserrat', sans-serif",
+    h1: {
+      fontWeight: 500,
+      fontSize: '3rem',
+      color: '#18181b',
+      ['@media (max-width:600px)']: {
+        fontSize: "1.9rem",
+      }
+    },
+    h2: {
+      fontWeight: 600,
+      fontSize: '1.5rem',
+      color: '#18181b',
+    },
+    h3: {
+      fontWeight: 600,
+      fontSize: '1.5rem',
+      color: '#18181b',
+    },
+    body1: {
+      color: '#18181b',
+    }
+  },
+})
+
+export default theme;
