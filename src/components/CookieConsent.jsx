0ec487f2974dc74a6db3d4a311b953813972b805
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2025-02-17 14:48:05 +0100

    feat => cookies permission

--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -1,35 +1,42 @@
 import { useState, useEffect } from "react";
 import { Box, Button, Typography, Snackbar, Paper } from "@mui/material";
+import Cookies from "js-cookie";
+import cookie from "../assets/img/cookie.webp";
+import { useTranslation } from "react-i18next";
+import { reloadI18n } from "../i18n/i18n";
 
 const COOKIE_CONSENT_KEY = "cookiesConsent";
 
 const CookieConsent = () => {
   const [open, setOpen] = useState(false);
 
+  const {t} = useTranslation();
+
   useEffect(() => {
-    const consent = localStorage.getItem(COOKIE_CONSENT_KEY);
-    if (!consent || consent === "false") {
+    const consent = Cookies.get(COOKIE_CONSENT_KEY);
+    if (!consent) {
       setOpen(true);
     }
   }, []);
 
   const handleAccept = () => {
-    localStorage.setItem(COOKIE_CONSENT_KEY, "true"); 
+    Cookies.set(COOKIE_CONSENT_KEY, "true", { expires: 365 });
+    reloadI18n();
     setOpen(false);
   };
 
   const handleDecline = () => {
-    localStorage.setItem(COOKIE_CONSENT_KEY, "false"); 
+    Cookies.set(COOKIE_CONSENT_KEY, "false", { expires: 365 });
     setOpen(false);
   };
 
   return (
     <Snackbar
       open={open}
-      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
     >
       <Paper
-        elevation={3}
+        elevation={24}
         sx={{
           p: 2,
           display: "flex",
@@ -37,18 +44,38 @@ const CookieConsent = () => {
           alignItems: "center",
           textAlign: "center",
           maxWidth: 400,
+          position: 'relative',
         }}
       >
-        <Typography variant="body1">
-          We use cookies to improve your experience. Accept?
+        <Typography variant="body1" sx={{pt: '1rem'}}>
+          {t("We use cookies to improve your experience")}<br />{t("Accept")}&nbsp;?
         </Typography>
         <Box sx={{ mt: 2, display: "flex", gap: 1 }}>
-          <Button variant="contained" color="primary" onClick={handleAccept}>
-            Accept
+          <Button variant="contained" color="success" onClick={handleAccept}>
+            {t("Accept")}
           </Button>
-          <Button variant="outlined" color="secondary" onClick={handleDecline}>
-            Decline
+          <Button variant="outlined" color="error" onClick={handleDecline}>
+            {t("Decline")}
           </Button>
+        </Box>
+        <Box
+          sx={{
+            width: "100px",
+            height: '100px',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: 'absolute',
+            top: "-60px",
+            left: '50%',
+            transform: 'translateX(-50%)',
+          }}
+        >
+          <img
+            src={cookie}
+            alt="cookie"
+            style={{ width: "100%", objectFit: "cover" }}
+          />
         </Box>
       </Paper>
     </Snackbar>
--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,34 +1,47 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Cookies from "js-cookie";
 
-import enTranslation from './locales/en.json';
-import frTranslation from './locales/fr.json';
+import enTranslation from "./locales/en.json";
+import frTranslation from "./locales/fr.json";
 
 function userConsentedToCookies() {
-  return localStorage.getItem("cookiesConsent") === "true"; 
+  return Cookies.get("cookiesConsent") === "true";
 }
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: enTranslation
+const initI18n = () => {
+  i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      resources: {
+        en: {
+          translation: enTranslation,
+        },
+        fr: {
+          translation: frTranslation,
+        },
       },
-      fr: {
-        translation: frTranslation
-      }
-    },
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false
-    },
-    detection: {
-      order: ['cookie', 'localStorage', 'navigator'],
-      caches: userConsentedToCookies() ? ['cookie'] : [],
-    }
+      fallbackLng: "en",
+      interpolation: {
+        escapeValue: false,
+      },
+      detection: {
+        order: userConsentedToCookies()
+          ? ["cookie", "localStorage", "navigator"]
+          : ["localStorage", "navigator"],
+        caches: userConsentedToCookies() ? ["cookie"] : [],
+      },
+    });
+};
+
+initI18n();
+
+export const reloadI18n = () => {
+  i18n.reloadResources().then(() => {
+    initI18n();
   });
+}
 
 export default i18n;
