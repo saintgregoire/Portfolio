e6687f300f783d65b107e5d3a285d146dbd6b1c0
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2025-02-17 19:30:46 +0100

    feat => imageOptimizer added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,31 @@ export default defineConfig({
       plugins: [autoprefixer],
     },
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      svg: {
+        plugins: [
+          "removeDoctype",
+          "removeXMLProcInst",
+          "minifyStyles",
+          "sortAttrs",
+          "sortDefsChildren",
+        ],
+      },
+      png: {
+        quality: 70,
+      },
+      jpeg: {
+        quality: 70,
+      },
+      jpg: {
+        quality: 70
+      },
+      webp: {
+        quality: 70,
+        lossless: true
+      }
+    }),
+  ],
+});
